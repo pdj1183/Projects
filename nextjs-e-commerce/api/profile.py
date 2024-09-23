48a138634ae6398b9d8dab54f5d19d62e8ed2bb7
Phillip Johnson <pdj1183@gmail.com>
2024-09-23 16:15:22 -0600

    sql grab

--- nextjs-e-commerce/api/profile.py
+++ nextjs-e-commerce/api/profile.py
@@ -1,14 +1,20 @@
-from flask_restx import Resource, reqparse
-from flask_jwt_extended import jwt_required
+from flask_restx import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from api.db import get_db
 
 class profile(Resource):
+
     @jwt_required()
     def get(self):
+        db = get_db()
+        data = db.execute(
+                'SELECT * FROM user WHERE username = ?',
+                [get_jwt_identity()]
+                ).fetchone()
         resposnse_body = {
-                "name": "Phillip",
-                "about": "TEST"
-                }
+                "name": data["username"],
+                "email": data["email"],
+                                }
         return resposnse_body
 
