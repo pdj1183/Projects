354bd36c98186f7f4a1d6a8228345c4d2cbc147f
Phillip Johnson <pdj1183@gmail.com>
2024-09-23 10:57:14 -0600

    Token Refresh

--- nextjs-e-commerce/api/auth.py
+++ nextjs-e-commerce/api/auth.py
@@ -43,6 +43,22 @@ class userDAO(object):
 DAO = userDAO()
 
 SECRET_KEY='MY-big-SECRET'
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = jwt.decode(response, )
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token 
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
 
 
 class login(Resource):
