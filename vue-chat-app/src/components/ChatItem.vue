100c1aa642106bdf11d1f773a7c4e99c4721b57c
Phillip Johnson <pdj1183@gmail.com>
2024-10-02 10:08:57 -0600

    Chat view

---
+++ vue-chat-app/src/components/ChatItem.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+    name: String;
+    selected: Boolean;
+}>();
+</script>
+
+<template>
+    <div class="item" :class="{ selected: selected }">
+        <p>{{ name }}</p>
+    </div>
+</template>
+
+<style scoped>
+.item {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 90%;
+    height: 50px;
+    text-indent: 15px;
+    border: lightgrey 1px ;
+    border-style: none none solid;
+}
+
+.selected {
+    background-color: rgba(194, 70, 70, 0.82);
+    border-radius: 20px;
+}
+</style>
--- vue-chat-app/src/components/ChatList.vue
+++ vue-chat-app/src/components/ChatList.vue
@@ -1,12 +1,30 @@
+<script setup lang="ts">
+import ChatItem from "./ChatItem.vue";
+</script>
 <template>
-    <ul class="list">
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-    </ul>
-    
+    <div class="list">
+        <input class="search" placeholder="search" />
+        <br />
+        <ChatItem name="Test Name" selected="{true}" />
+        <ChatItem name="Bob Name" />
+        <ChatItem name="Tim the guy" />
+        <ChatItem name="Who knows Name" />
+    </div>
 </template>
 
 <style scoped>
+.search {
+    height: 25px;
+    width: 90%;
+    border-radius: 10px;
+    text-indent: 10px;
+    border-style: solid;
+}
+.list {
+    display: flex;
+    flex-direction: column;
 
+    padding: 10px;
+    align-items: center;
+}
 </style>
